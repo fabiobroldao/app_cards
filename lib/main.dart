70331367ed40265b7860be0adf8596f83e4a7e26
Ticksoares <patrick.soares.dias@gmail.com>
2021-03-29 19:15:00 -0300

    teteet

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.white,
       ),
       home: LoginPage(),
     );
